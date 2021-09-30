a4f84060c9db6fb5ca8f8b1a37fc297f0f5c2b0c
Rishabh Agarwal <arishabh106@gmail.com>
2021-09-30 17:50:58 +0530

    Code and Results Added

---
+++ test.py
@@ -0,0 +1,50 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+import cv2
+from data import load_data, tf_dataset
+from model import build_unet
+
+H = 256
+W = 256
+num_classes = 3
+
+if __name__ == "__main__":
+    np.random.seed(42)
+    tf.random.set_seed(42)
+    path = "./DataSet/"
+    (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(path)
+    print(f"Dataset: Train: {len(train_x)} - Valid: {len(valid_x)} - Test: {len(test_x)}")
+    model = tf.keras.models.load_model("./model.h5/")
+    for x, y in tqdm(zip(test_x, test_y), total=len(test_x)):
+        name = x.split("/")[-1]
+        x = cv2.imread(x, cv2.IMREAD_COLOR)
+        x = cv2.resize(x, (W, H))
+        x = x / 255.0
+        x = x.astype(np.float32)
+
+        y = cv2.imread(y, cv2.IMREAD_GRAYSCALE)
+        y = cv2.resize(y, (W, H))  
+        y = y - 1
+        y = np.expand_dims(y, axis=-1) 
+        y = y * (255/num_classes)
+        y = y.astype(np.int32)
+        y = np.concatenate([y, y, y], axis=2)
+
+        p = model.predict(np.expand_dims(x, axis=0))[0]
+        p = np.argmax(p, axis=-1)
+        p = np.expand_dims(p, axis=-1)
+        p = p * (255/num_classes)
+        p = p.astype(np.int32)
+        p = np.concatenate([p, p, p], axis=2)
+
+        x = x * 255.0
+        x = x.astype(np.int32)
+
+        h, w, _ = x.shape
+        line = np.ones((h, 10, 3)) * 255
+
+
+        final_image = np.concatenate([x, line, y, line, p], axis=1)
+        cv2.imwrite(f"./results/{name}", final_image)
